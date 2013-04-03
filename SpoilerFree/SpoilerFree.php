59dfc0b7de48910bf23ba3277216bda7546df694
litex2x <ritchlolz@gmail.com>
2013-04-03 13:19:10 -0700

    Added internationalization

--- SpoilerFree/SpoilerFree.php
+++ SpoilerFree/SpoilerFree.php
@@ -1,18 +1,25 @@
 <?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo <<<EOT
+To install my extension, put the following line in LocalSettings.php:
+require_once( "$IP/extensions/SpoilerFree/SpoilerFree.php" );
+EOT;
+	exit( 1 );
+}
+
 $wgExtensionCredits['validextensionclass'][] = array(
 		'path' => __FILE__,
 		'name' => 'SpoilerFree',
 		'author' => 'Ritchie Marihugh',
-		'url' => 'http://www.google.com',
+		'url' => 'https://github.com/litex2x/SpoilerFree',
 		'description' => 'This extension allows users to filter mediawiki pages to remain spoiler free.',
 		'version' => '1.0.0'
 );
 
 $dir = dirname(__FILE__);
-
 $wgAutoloadClasses['SpoilerFreeHooks'] = $dir . '/SpoilerFree.hooks.php';
 $wgAutoloadClasses['SpecialSeasonsSelection'] = $dir . '/specials/SpecialSeasonsSelection.php';
-
+$wgExtensionMessagesFiles['SpoilerFree'] = $dir . '/SpoilerFree.i18n.php';
 $wgHooks['ParserFirstCallInit'][] = 'SpoilerFreeHooks::sfParserInit';
 $wgSpecialPages['SeasonsSelection'] = 'SpecialSeasonsSelection';
 $wgSpecialPageGroups['SeasonsSelection'] = 'other';
