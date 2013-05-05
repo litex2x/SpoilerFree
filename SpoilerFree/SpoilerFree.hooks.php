87371e61636fe6c450adf2b4fc68e5cd715ee971
litex2x <ritchlolz@gmail.com>
2013-05-05 12:45:00 -0700

    Changed behavior for users who aren't logged in. Everything will show for them.

--- SpoilerFree/SpoilerFree.hooks.php
+++ SpoilerFree/SpoilerFree.hooks.php
@@ -8,16 +8,20 @@ class SpoilerFreeHooks {
 
 	public static function sfRender ( $input, array $args, Parser $parser, PPFrame $frame ) {
 		global $wgUser;
-
 		$parser->disableCache();
-		$season = $wgUser->getOption('season');
-		$episode = $wgUser->getOption('episode');
 
-		if($args['id'] < $season || $args['id'] == $season && $args['episode'] <= $episode) {
+		if(!$wgUser->isLoggedIn()){
 			return $parser->recursiveTagParse( $input, $frame );
+		} else {
+			$season = $wgUser->getOption('season');
+			$episode = $wgUser->getOption('episode');
+	
+			if($args['id'] < $season || $args['id'] == $season && $args['episode'] <= $episode) {
+				return $parser->recursiveTagParse( $input, $frame );
+			}
+			
+			return '';
 		}
-		
-		return '';
 	}
 }
 ?>
